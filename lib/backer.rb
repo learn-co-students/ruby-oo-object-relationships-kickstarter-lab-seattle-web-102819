f25a293d29325df4cda849ee8fdefdfa8f047ba1
bahayg <bahayg@gmail.com>
2019-11-01 13:45:08 -0700

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+class Backer
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+
+    def backed_projects
+        ProjectBacker.all.select {|p| p.backer == self}.map {|proj| proj.project}
+    end
+
+end
