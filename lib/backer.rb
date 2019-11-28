081fda24f9e34c49ba6a97a7a211edca6021a28d
4myc <missch3n@gmail.com>
2019-11-27 23:16:28 -0800

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -16,24 +16,24 @@ class Backer
     end
 
     def backed_projects
-        ProjectBacker.all.select {|project| project.backer == self}.map {|project| project.project}
+        ProjectBacker.all.select {|bproj| bproj.backer == self}.map {|project| project.project}
     end
 
     # ALTERNATIVE METHOD #1
     # def backed_projects
-    #     ProjectBacker.all.select {|project| project.backer == self}
+    #     ProjectBacker.all.select {|bproj| bproj.backer == self}
     # end.map {|project| project.project}
 
     # ALTERNATIVE METHOD #2
     # def backed_projects
-    #     project_backers = ProjectBacker.all.select {|project| project.backer == self}
+    #     project_backers = ProjectBacker.all.select {|bproj| bproj.backer == self}
     #     project_backers.map {|project| project.project}
     # end
 
     # ALTERNATIVE METHOD #3
     # def backed_projects
-    #     project_backers = ProjectBacker.all.select do |project|
-    #         project.backer == self
+    #     project_backers = ProjectBacker.all.select do |bproj|
+    #         bproj.backer == self
     #     end
     #     project_backers.map do |project|
     #         project.project
--- lib/project.rb
+++ lib/project.rb
@@ -16,24 +16,24 @@ class Project
     end
 
     def backers
-        ProjectBacker.all.select {|project| project.project == self}.map {|project| project.backer}
+        ProjectBacker.all.select {|bproj| bproj.project == self}.map {|project| project.backer}
     end
 
     # ALTERNATIVE METHOD #1
     # def backers
-    # ProjectBacker.all.select do |bp| bp.project == self
+    # ProjectBacker.all.select do |bproj| bproj.project == self
     # end.map {|project| project.backer}
     
     # ALTERNATIVE METHOD #2
     # def backers
-    #     project_backers = ProjectBacker.all.select {|project| project.project == self}
+    #     project_backers = ProjectBacker.all.select {|bproj| bproj.project == self}
     #     project_backers.map {|project| project.backer}
     # end
 
     # ALTERNATIVE METHOD #3
     # def backers
-    #     project_backers = ProjectBacker.all.select do |project|
-    #         project.project == self
+    #     project_backers = ProjectBacker.all.select do |bproj|
+    #         bproj.project == self
     #     end
     #     project_backers.map do |project|
     #         project.backer
